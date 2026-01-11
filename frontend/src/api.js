77840b45238141edd7e49e9f725e267e347c0125
rocky0shao <rockyshao22@gmail.com>
2026-01-11 17:30:21 -0500

    protect view & basic frontend routing

---
+++ frontend/src/api.js
@@ -0,0 +1,23 @@
+//interceptor => auto add hearder for requrests
+import axios from 'axios';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from './constants';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL 
+});
+
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
